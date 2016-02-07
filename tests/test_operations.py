4f0f2088e2cf3ddd6592479bbaf7bb9711648471
Brian Hammond <brian@fictorial.com>
2016-02-06 22:34:37 -0500

    Add more update operations.
    add_to_set, remove_from_set, append (item to list), prepend (item to list), slice.
    Also modifies increment and decrement to take deltas instead of hard-coding 1.
    decrement can now raise ValueError if the value of the field would become negative.

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,5 +1,8 @@
 from tinydb import where
-from tinydb.operations import delete, increment, decrement
+from tinydb.operations import delete, increment, decrement, \
+    add_to_set, remove_from_set, append, prepend, slice
+
+import pytest
 
 
 def test_delete(db):
@@ -15,3 +18,83 @@ def test_increment(db):
 def test_decrement(db):
     db.update(decrement('int'), where('char') == 'a')
     assert db.get(where('char') == 'a')['int'] == 0
+
+
+def test_increment_with_delta(db):
+    db.update(increment('int', 9), where('char') == 'a')
+    assert db.get(where('char') == 'a')['int'] == 10
+
+
+def test_decrement_with_delta(db):
+    db.update(decrement('int', 2), where('char') == 'a')
+    assert db.get(where('char') == 'a')['int'] == -1
+
+
+def test_decrement_with_delta_raise(db):
+    with pytest.raises(ValueError):
+        db.update(decrement('int', 2, raise_if_negative=True), where('char') == 'a')
+
+
+def test_add_to_set(db):
+    db.update(add_to_set('set', 'x'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['set'] == ['x']
+
+    db.update(add_to_set('set', 'x'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['set'] == ['x']
+
+    db.update(add_to_set('set', 'y'), where('char') == 'a')
+    assert sorted(db.get(where('char') == 'a')['set']) == ['x','y']
+
+
+def test_remove_from_set(db):
+    db.update(add_to_set('set', 'x'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['set'] == ['x']
+
+    db.update(remove_from_set('set', 'x'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['set'] == []
+
+    db.update(add_to_set('set', 'x'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['set'] == ['x']
+
+    db.update(add_to_set('set', 'y'), where('char') == 'a')
+    assert sorted(db.get(where('char') == 'a')['set']) == ['x','y']
+
+    db.update(remove_from_set('set', 'y'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['set'] == ['x']
+
+
+def test_append(db):
+    db.update(append('list', 'x'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['list'] == ['x']
+
+    db.update(append('list', 'x'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['list'] == ['x','x']
+
+    db.update(append('list', 'y'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['list'] == ['x','x','y']
+
+
+def test_prepend(db):
+    db.update(prepend('list', 'x'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['list'] == ['x']
+
+    db.update(prepend('list', 'y'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['list'] == ['y','x']
+
+    db.update(prepend('list', 'z'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['list'] == ['z','y','x']
+
+
+def test_slice(db):
+    db.update(prepend('list', 'x'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['list'] == ['x']
+
+    db.update(prepend('list', 'y'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['list'] == ['y','x']
+
+    db.update(prepend('list', 'z'), where('char') == 'a')
+    assert db.get(where('char') == 'a')['list'] == ['z','y','x']
+
+    db.update(slice('list', 0, 2), where('char') == 'a')
+    assert db.get(where('char') == 'a')['list'] == ['z','y']
+
--- tinydb/operations.py
+++ tinydb/operations.py
@@ -8,23 +8,91 @@ def delete(field):
     return transform
 
 
-def increment(field):
+def increment(field, delta=1):
     """
-    Increment a given field in the element.
+    Increment a given field in the element
+    by the given delta.
     """
     def transform(element):
         element.setdefault(field, 0)
-        element[field] += 1
+        element[field] += delta
 
     return transform
 
 
-def decrement(field):
+def decrement(field, delta=1, raise_if_negative=False):
     """
-    Decrement a given field in the element.
+    Decrement a given field in the element by the
+    given delta.
+
+    If `raise_if_negative` and the value at field _would_
+    become negative, raise `ValueError`.
     """
     def transform(element):
-        element.setdefault(field, 0)
-        element[field] -= 1
+        value = element.setdefault(field, 0)
+
+        if raise_if_negative and value - delta < 0:
+            raise ValueError(field)
+
+        element[field] = value - delta
+
+    return transform
+
+
+def add_to_set(field, member):
+    """
+    Add a member to a set at field in the element.
+    """
+    def transform(element):
+        members = set(element.setdefault(field, []))
+        members.add(member)
+        element[field] = list(members)
+
+    return transform
+
+
+def remove_from_set(field, member):
+    """
+    Remove a member from a set at field in the element.
+    """
+    def transform(element):
+        if field in element:
+            members = set(element[field])
+            members.remove(member)
+            element[field] = list(members)
+
+    return transform
+
+
+def append(field, item):
+    """
+    Add an item to the end of the list at field
+    in the element.
+    """
+    def transform(element):
+        element.setdefault(field, []).append(item)
+
+    return transform
+
+
+def prepend(field, item):
+    """
+    Add an item to the front of the list at field
+    in the element.
+    """
+    def transform(element):
+        element.setdefault(field, []).insert(0, item)
+
+    return transform
+
+
+def slice(field, start, end):
+    """
+    Slice the list at field in the element such that
+    the items in range `[start, end)` remain.
+    """
+    def transform(element):
+        if field in element and type(element[field]) == list:
+            element[field] = element[field][start:end]
 
     return transform
